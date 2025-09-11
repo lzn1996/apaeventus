a2a21cb79f14096e9b6347179ef7bd74a43ab8c5
Rafael Costa <rafaelcosta8020@gmail.com>
2025-09-11 19:54:04 -0300

    refactor: remove unnecessary comment from TicketsByEventScreen

--- src/screens/TicketsByEventScreen/index.tsx
+++ src/screens/TicketsByEventScreen/index.tsx
@@ -128,7 +128,6 @@ export default function TicketsByEventScreen({route}: any) {
                 total={localTickets.length}
               />
             )}
-            // modo padrão, sem stack nem triângulos:
             pagingEnabled
             snapEnabled
             autoPlay={false}
